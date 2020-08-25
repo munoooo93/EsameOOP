81271aca8726b05c10a287ee2a5bd9a57d4ee82e
Emanuele Ballarini <ema.balla@hotmail.it>
2020-08-25 16:45:33 +0200

    Initial Commit

---
+++ src/main/java/it/esame/EsameOOP/EsameOopApplication.java
@@ -0,0 +1,13 @@
+package it.esame.EsameOOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsameOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsameOopApplication.class, args);
+	}
+
+}
