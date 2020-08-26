9198dfc29746fc1e96c03b5826201206989cbdfe
Emanuele Ballarini <ema.balla@hotmail.it>
2020-08-26 15:37:38 +0200

    Modified FileInfo: added more attributes and methods

--- src/main/java/it/esame/EsameOOP/model/FileInfo.java
+++ src/main/java/it/esame/EsameOOP/model/FileInfo.java
@@ -1,33 +1,81 @@
 package it.esame.EsameOOP.model;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class FileInfo {
-	private String name;
-	private String path;
-	private long size;
+	private String	name;
+	private String	path;
+	private long	size;
+	private boolean	deleted;
+	private boolean downloadable;
 	
-	public FileInfo(String name, String path, long size) {
+	public FileInfo(String name, String path, long size, boolean deleted, boolean downloadable) {
 		super();
 		this.name = name;
 		this.path = path;
 		this.size = size;
+		this.deleted = deleted;
+		this.downloadable = downloadable;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPath() {
 		return path;
 	}
+
 	public void setPath(String path) {
 		this.path = path;
 	}
+
 	public long getSize() {
 		return size;
 	}
+
 	public void setSize(long size) {
 		this.size = size;
+	}
+
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	public boolean isDownloadable() {
+		return downloadable;
+	}
+
+	public void setDownloadable(boolean downloadable) {
+		this.downloadable = downloadable;
+	}
+	
+	
+	public JSONObject getFileBasicInfo() throws JSONException {
+		JSONObject basicInfo = new JSONObject();
+		
+		/*
+		 * presenza
+		 * dimensioni
+		 * 
+		 * cancellati (relative dimensioni)
+		 * 
+		 **/
+		
+		basicInfo.put("name", this.getName());
+		basicInfo.put("path", this.getPath());
+		basicInfo.put("deleted", this.isDeleted());
+		basicInfo.put("size", this.getSize());
+		
+		return basicInfo;
 	}
 }
